41bbbe1d2e1daa603fbe888956c7cb77d7f494ea
Daniellee17 <danielleespiritu17@gmail.com>
2020-01-31 00:29:47 -0500

    32

---
+++ thesis/mainApp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+#ONE CLASS ONE TABLE
+
+class sensors(models.Model):
+    temperature = models.FloatField(max_length=250, default=0.0);
+    humidity = models.FloatField(max_length=250, default=0.0);
+    moisture = models.IntegerField(default=3);
+    summary = models.TextField(max_length=250, default="None");
+    date = models.DateTimeField(auto_now=True);
+
+
+class devicestatus(models.Model):
+    fansStatus = models.TextField(max_length=250, default="off");
+    lightsStatus = models.TextField(max_length=250, default="off");
+    waterStatus = models.TextField(max_length=250, default="off");
+    seedStatus = models.TextField(max_length=250, default="off");
+    date = models.DateTimeField(auto_now=True);
+
+class camerasnaps(models.Model):
+    camera = models.ImageField(default='default.png', blank=True);
+    cameraURL = models.ImageField(default='../assets/gardenPics/default.png', blank=True); 
+    date = models.DateTimeField(auto_now=True);
